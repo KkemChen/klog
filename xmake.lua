60566b563ebb9e595e8d9dc939c5f40f929d5746
KkemChen <44298896+KkemChen@users.noreply.github.com>
2023-05-16 01:03:08 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,37 +1,10 @@
-set_project("demo", "this is a demo")
-set_xmakever("2.7.8")
-set_symbols("debug")
-add_cxxflags("-std=c++14")
-add_cxxflags("-Wall","-DSPDLOG_COMPILED_LIB")
+add_rules("mode.debug", "mode.release")
+set_languages("cxx14")
+add_cxxflags("-Wall")
+add_defines("SPDLOG_COMPILED_LIB")
 
-if is_plat("linux") then
-    add_ldflags("-pthread")
-elseif is_plat("windows") then
-    add_ldflags("-mwindows")
-end
 
-target("demo")
+target("test")
     set_kind("binary")
-    add_deps("spdlog")
-    set_targetdir("./build")
-    add_files("./*.cpp")
     add_includedirs("include")
-    add_links("stdc++fs","spdlog")
-    
-target("spdlog")
-    set_kind("shared")
-    set_targetdir("./build")
-    add_includedirs("include")
-    add_files("./src/*.cpp")
-    add_files("Log.cpp")
-
-
---target("install")
-    --set_kind("phony")
-    --add_deps("demo","spdlog")
-    --on_install(function (target)
-        --os.cp("build/demo", "bin/")
-        --os.cp("include/*", "bin/include/")
-        --os.cp("lib/*", "bin/lib/")
-    --end)
-
+    add_files("src/main.cpp","src/Log.cpp","include/spdlog/src/*.cpp")
